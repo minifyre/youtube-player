556be5d36831cf3e0a31866c21dcf902302ea992
Minifyre <minifyre@gmail.com>
2018-09-22 17:07:13 -0500

    modularized config & output

---
+++ config.mjs
@@ -0,0 +1,8 @@
+const config={}
+config.api='https://www.youtube.com/iframe_api'
+config.state=
+{
+	time:0,
+	video_id:'PUv66718DII'
+}
+export default config
--- index.mjs
+++ index.mjs
@@ -1,9 +1,11 @@
-import util from './util.mjs'
-const
-config={},
-output={}
-
-config.api='https://www.youtube.com/iframe_api'
+import {config,output,util} from './output.mjs'
+const input={}
+input.state=function(viewer)
+{
+	viewer.state.video_id=viewer.player.getVideoData().video_id,
+	viewer.state.time=viewer.player.getCurrentTime()
+	output.event(viewer,'pause',{time:viewer.time})
+}
 export default async function youtube(url='/node_modules/youtube-viewer/')
 {
 	const {error}=await new Promise(async function(res,rej)
@@ -21,26 +23,6 @@ export default async function youtube(url='/node_modules/youtube-viewer/')
 	config.dom=`<style>${css}</style>${html}`
 	customElements.define('youtube-player',youtube.player)
 }
-output.player=function(el)
-{
-	return new util.yt.Player(el,
-	{
-		height:'390',
-		width:'640',
-		videoId:'PUv66718DII',
-		events:
-		{
-			'onReady':function({target})
-			{
-				target.playVideo()
-			},
-			'onStateChange':function({data})
-			{
-				//if (data==YT.PlayerState.PLAYING) 'do something'
-			}
-		}
-	})
-}
 youtube.player=class extends HTMLElement
 {
 	constructor()
@@ -49,16 +31,12 @@ youtube.player=class extends HTMLElement
 		const shadow=this.attachShadow({mode:'open'})
 		shadow.innerHTML=config.dom
 		this.player=null
-		this.state=//@todo wrap in config
-		{
-			time:0,
-			video_id:'PUv66718DII'
-		}
+		this.state=Object.assign({},config.state)
 	}
-	connectedCallback()
+	connectedCallback()//@todo find a less hackish way to do this
 	{
 		const iframe=this.shadowRoot.querySelector('#youtube-player')
-		document.body.append(iframe)
+		document.body.append(iframe)//moved outside shadow dom to initalize
 		this.player=output.player(iframe)
 		this.shadowRoot.append(document.querySelector('#youtube-player'))
 	}
