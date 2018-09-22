030f2192ba3e625983226565ec58fd0d9ea5e796
Minifyre <minifyre@gmail.com>
2018-09-22 16:28:59 -0500

    moved player into custom element
    Iframe does not want to initate outside of the shadow dom. So it starts outside the shadow dom, is initalized, and is then brought into the custom element.

--- index.mjs
+++ index.mjs
@@ -1,16 +1,29 @@
 import util from './util.mjs'
-const config={}
+const
+config={},
+output={}
+
 config.api='https://www.youtube.com/iframe_api'
-export default async function youtube()
+export default async function youtube(url='/node_modules/youtube-viewer/')
 {
 	const {error}=await new Promise(async function(res,rej)
 	{
 		window.onYouTubeIframeAPIReady=()=>res({})
-		const {error}= await util.loadScript(config.api)
+		const {error}=await util.loadScript(config.api)
 		if(error) rej({error})
 	})
 	if(error) return {error}
-	const player=new YT.Player(document.querySelector('#player'),
+	util.yt=window.YT
+	const
+	importFile=path=>fetch(path).then(x=>x.text()),
+	files=['css','html'].map(ext=>url+'index.'+ext),
+	[css,html]=await Promise.all(files.map(importFile))
+	config.dom=`<style>${css}</style>${html}`
+	customElements.define('youtube-player',youtube.player)
+}
+output.player=function(el)
+{
+	return new util.yt.Player(el,
 	{
 		height:'390',
 		width:'640',
@@ -27,4 +40,33 @@ export default async function youtube()
 			}
 		}
 	})
+}
+youtube.player=class extends HTMLElement
+{
+	constructor()
+	{
+		super()
+		const shadow=this.attachShadow({mode:'open'})
+		shadow.innerHTML=config.dom
+		this.player=null
+		this.state=//@todo wrap in config
+		{
+			time:0,
+			video_id:'PUv66718DII'
+		}
+	}
+	connectedCallback()
+	{
+		const iframe=document.querySelector('#youtube-player')
+		this.player=output.player(iframe)
+		this.shadowRoot.append(document.querySelector('#youtube-player'))
+	}
+	adoptedCallback()
+	{
+		console.error('add adoptedCallback behavior')
+	}
+	disconnectedCallback()
+	{
+		console.error('add disconnectedCallback behavior')
+	}
 }
